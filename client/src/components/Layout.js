3763cf6004a399abad68577e646121ff982e2250
Alexander Tatchin <alexander.tatchin@gmail.com>
2016-09-09 13:03:57 +0300

    added temp link to dep-73 calc service

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -6,7 +6,12 @@ import './Layout.css';
 class Layout extends Component {
   render() {
     return (
-      <div className="Layout">
+      <div className='layout'>
+        <div className='navigation'>
+          <a href='http://sqlisp:9000/app'>
+            <button className='btn btn-default btn-sm'>Расчёт трудоёмкости</button>
+          </a>
+        </div>
         {this.props.children}
       </div>
     );
